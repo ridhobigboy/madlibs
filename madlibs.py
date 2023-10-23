3f77334f9d06661cb47a5f4df694c0678103e17b
Muhammad Ridho Fadhillah <muhammadridhofadhillah@192.168.1.9>
2023-10-23 16:34:49 +0700

    madlibs

---
+++ madlibs.py
@@ -0,0 +1,9 @@
+adj = input("adjective: ")
+verb1 = input("Verb1: ")
+verb2 = input("Verb2: ")
+famous_person = input("famous person: ")
+
+madlib = f"computer programming is so {adj}! its make me so excited all the time because \
+    ilove to {verb1}, stay hydrated and {verb2} like you are {famous_person}"
+
+print(madlib)
